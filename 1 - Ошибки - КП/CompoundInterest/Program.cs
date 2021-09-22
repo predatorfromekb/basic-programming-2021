283c3f4e7e9eec100da75cff3cb69b86858fac1d
Stepan Ermakov <stepanus97@yandex.ru>
2021-09-22 22:16:41 +0500

    add CompoundInterest and Risovatel

---
+++ 1 - Ошибки - КП/CompoundInterest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CompoundInterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate(Console.ReadLine());
+        }
+        
+        public static double Calculate(string userInput)
+        {
+
+            return 0;
+        }
+    }
+}
