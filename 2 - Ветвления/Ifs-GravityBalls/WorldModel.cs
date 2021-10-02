0bf1bc57ccbb50a1234843e9267ce35d94d52719
Stepan Ermakov <stepanus97@yandex.ru>
2021-10-02 21:04:02 +0500

    Add GravityBalls

---
+++ 2 - Ветвления/Ifs-GravityBalls/WorldModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GravityBalls
+{
+	public class WorldModel
+	{
+		public double BallX;
+		public double BallY;
+		public double BallRadius;
+		public double WorldWidth;
+		public double WorldHeight;
+
+		public void SimulateTimeframe(double dt)
+		{
+			BallY = Math.Min(BallY + 200*dt, WorldHeight - BallRadius);
+		}
+	}
+}
