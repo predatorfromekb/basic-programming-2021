c4e9e351a023fe631c0e1edf47ef1c21c02a3e8a
Stepan Ermakov <stepanus97@yandex.ru>
2021-10-13 22:18:45 +0500

    Names - code

--- 4 - Массивы/Names/HeatmapTask.cs
+++ 4 - Массивы/Names/HeatmapTask.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Names
 {
@@ -8,9 +9,38 @@ namespace Names
         {
             return new HeatmapData(
                 "Пример карты интенсивностей",
-                new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-                new[] { "a", "b", "c", "d" }, 
-                new[] { "X", "Y", "Z" });
+                GenerateData(names), 
+                GenerateLabelsX(), 
+                GenerateLabelsY());
+        }
+
+        private static double[,] GenerateData(NameData[] names)
+        {
+            var array = new double[30, 12];
+            foreach (var name in names)
+            {
+                if (name.BirthDate.Day == 1)
+                    continue;
+                array[name.BirthDate.Day - 2, name.BirthDate.Month - 1]++;
+            }
+
+            return array;
+        }
+        
+        private static string[] GenerateLabelsX()
+        {
+            return Enumerable
+                .Range(2, 30)
+                .Select(e => e.ToString())
+                .ToArray();
+        }
+        
+        private static string[] GenerateLabelsY()
+        {
+            return Enumerable
+                .Range(1, 12)
+                .Select(e => e.ToString())
+                .ToArray();
         }
     }
 }
--- 4 - Массивы/Names/HistogramTask.cs
+++ 4 - Массивы/Names/HistogramTask.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace Names
 {
@@ -8,9 +7,30 @@ namespace Names
         public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
         {
             return new HistogramData(
-                string.Format("Рождаемость людей с именем '{0}'", name), 
-                new string[0], 
-                new double[0]);
+                $"Рождаемость людей с именем '{name}'", 
+                GenerateLabels(), 
+                GenerateValues(names, name));
+        }
+        
+        private static double[] GenerateValues(NameData[] names, string currentName)
+        {
+            var array = new double[31];
+            foreach (var name in names)
+            {
+                if (name.Name != currentName || name.BirthDate.Day == 1)
+                    continue;
+                array[name.BirthDate.Day - 1]++;
+            }
+
+            return array;
+        }
+
+        private static string[] GenerateLabels()
+        {
+            return Enumerable
+                .Range(1, 31)
+                .Select(e => e.ToString())
+                .ToArray();
         }
     }
 }
