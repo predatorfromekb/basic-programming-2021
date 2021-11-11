2287da265304096f079ad547e6b7f7882636eb23
Stepan Ermakov <stepanus97@yandex.ru>
2021-11-11 12:44:04 +0500

    add Recursion

---
+++ 8 - Рекурсивные Алгоритмы/Passwords/CaseAlternatorTask.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Passwords
+{
+    public class CaseAlternatorTask
+    {
+        //Тесты будут вызывать этот метод
+        public static List<string> AlternateCharCases(string lowercaseWord)
+        {
+            var result = new List<string>();
+            AlternateCharCases(lowercaseWord.ToCharArray(), 0, result);
+            return result;
+        }
+
+        static void AlternateCharCases(char[] word, int startIndex, List<string> result)
+        {
+            // TODO
+            result.Add(new string (word));
+        }
+    }
+}
