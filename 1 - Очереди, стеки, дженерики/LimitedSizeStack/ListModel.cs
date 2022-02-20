e31ffecbb6fd834788a2b20d085d100e665860f3
Stepan Ermakov <stepanus97@yandex.ru>
2022-02-21 00:21:38 +0500

    LimitedSizeStack - solution (2)

--- 1 - Очереди, стеки, дженерики/LimitedSizeStack/ListModel.cs
+++ 1 - Очереди, стеки, дженерики/LimitedSizeStack/ListModel.cs
@@ -1,37 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace TodoApplication
 {
     public class ListModel<TItem>
     {
         public List<TItem> Items { get; }
-        public int Limit;
+        private readonly LimitedSizeStack<ICommand<TItem>> commands;
 
         public ListModel(int limit)
         {
             Items = new List<TItem>();
-            Limit = limit;
+            commands = new LimitedSizeStack<ICommand<TItem>>(limit);
         }
 
         public void AddItem(TItem item)
         {
-            Items.Add(item);
+            ExecuteAndPush(new AddCommand<TItem>(item));
         }
 
         public void RemoveItem(int index)
         {
-            Items.RemoveAt(index);
+            ExecuteAndPush(new RemoveCommand<TItem>(index));
+        }
+
+        private void ExecuteAndPush(ICommand<TItem> command)
+        {
+            command.Execute(Items);
+            commands.Push(command);
         }
 
         public bool CanUndo()
         {
-            return false;
+            return commands.Count > 0;
         }
 
         public void Undo()
         {
-            throw new NotImplementedException();
+            var command = commands.Pop();
+            command.Undo(Items);
+        }
+    }
+    
+    public interface ICommand<TItem>
+    {
+        void Execute(List<TItem> items);
+        void Undo(List<TItem> items);
+    }
+
+    public class AddCommand<TItem> : ICommand<TItem>
+    {
+        private readonly TItem item;
+
+        public AddCommand(TItem item)
+        {
+            this.item = item;
+        }
+
+        public void Execute(List<TItem> items)
+        {
+            items.Add(item);
+        }
+
+        public void Undo(List<TItem> items)
+        {
+            items.RemoveAt(items.Count - 1);
+        }
+    }
+    
+    public class RemoveCommand<TItem> : ICommand<TItem>
+    {
+        private TItem item;
+        private readonly int index;
+
+        public RemoveCommand(int index)
+        {
+            this.index = index;
+        }
+
+        public void Execute(List<TItem> items)
+        {
+            item = items.ElementAt(index);
+            items.RemoveAt(index);
+        }
+
+        public void Undo(List<TItem> items)
+        {
+            items.Insert(index, item);
         }
     }
 }
