24b5bf77ad90a22aa43262af0b6f79e59c4a4760
Stepan Ermakov <stepanus97@yandex.ru>
2022-03-08 16:40:55 +0500

    yield-return-smooth - solution 3/3

--- 2 - yield return/yield-return-smooth/MovingMaxTask.cs
+++ 2 - yield return/yield-return-smooth/MovingMaxTask.cs
@@ -1,16 +1,26 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace yield
 {
-
-	public static class MovingMaxTask
-	{
-		public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
-		{
-			//Fix me!
-			return data;
-		}
-	}
+    public static class MovingMaxTask
+    {
+        public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
+        {
+            var windowMaxs = new LinkedList<double>();
+            var window = new Queue<double>();
+            foreach (var dataPoint in data)
+            {
+                if (window.Count == windowWidth)
+                {
+                    var removed = window.Dequeue();
+                    if (windowMaxs.First.Value.Equals(removed)) windowMaxs.RemoveFirst();
+                }
+                while (windowMaxs.Count > 0 && windowMaxs.Last.Value < dataPoint.OriginalY)
+                    windowMaxs.RemoveLast();
+                windowMaxs.AddLast(dataPoint.OriginalY);
+                window.Enqueue(dataPoint.OriginalY);
+                yield return dataPoint.WithMaxY(windowMaxs.First.Value);
+            }
+        }
+    }
 }
