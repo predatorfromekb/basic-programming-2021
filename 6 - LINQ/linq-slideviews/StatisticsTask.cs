d6e7bcf551dffc5f9a32fb3e92327493aa4ed982
stepan ermakov <s.ermakov@skbkontur.ru>
2022-03-29 19:17:02 +0500

    linq-2

--- 6 - LINQ/linq-slideviews/StatisticsTask.cs
+++ 6 - LINQ/linq-slideviews/StatisticsTask.cs
@@ -8,7 +8,21 @@ namespace linq_slideviews
 	{
 		public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
 		{
-			throw new NotImplementedException();
+			var minutes = visits
+				.GroupBy(e => e.UserId)
+				.SelectMany(e =>
+				{
+					return e
+						.OrderBy(x => x.DateTime)
+						.Bigrams()
+						.Where(r => r.Item1.SlideType == slideType)
+						.Select(r => r.Item2.DateTime - r.Item1.DateTime)
+						.Where(time => time >= TimeSpan.FromMinutes(1) && time <= TimeSpan.FromHours(2));
+				})
+				.Select(e => e.TotalMinutes)
+				.ToList();
+
+			return minutes.Any() ? minutes.Median() : 0;
 		}
 	}
 }
