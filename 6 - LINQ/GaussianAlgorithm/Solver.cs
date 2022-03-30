02b1ff969a855247646b5273ef7e3b6b435a71ec
stepan ermakov <s.ermakov@skbkontur.ru>
2022-03-30 20:11:45 +0500

    add GaussianAlgorithm

---
+++ 6 - LINQ/GaussianAlgorithm/Solver.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+namespace GaussAlgorithm
+{
+    public class Solver
+    {
+        public double[] Solve(double[][] matrix, double[] freeMembers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
